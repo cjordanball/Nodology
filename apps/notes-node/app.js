953c4456c1fcc0577909e36abd710d9d871cc07a
C. Jordan Ball III <cjordanball@gmail.com>
2017-06-26 14:10:35 -0400

    updating sample node command-line program

--- apps/notes-node/app.js
+++ apps/notes-node/app.js
@@ -11,16 +11,32 @@ const command = process.argv[2];
 
 switch(command) {
 	case 'add':
-		notes.addNote(argv.title, argv.body);
+		let note = notes.addNote(argv.title, argv.body);
+		if (note) {
+			console.log(`Success! Title: ${note.title}, Body: ${note.body}`);
+		} else {
+			console.log('Please give the note a unique title.');
+		}
 		break;
 	case 'list':
 		console.log(notes.getAll());
 		break;
 	case 'read':
-		console.log(notes.getNote(argv.title));
+		let res = notes.getNote(argv.title);
+		if (res) {
+			console.log(`Title: ${res.title}, Body: ${res.body}`);
+		} else {
+			console.log(`Unable to locate note with title of ${argv.title}.`)
+		}
+
 		break;
 	case 'remove':
-		notes.removeNote(argv.title);
+		let removeRes = notes.removeNote(argv.title);
+		if (res) {
+			console.log('Note deleted');
+		} else {
+			console.log('There was a problem - no such note.');
+		}
 		break;
 	default:
 		console.log('command not recognized');
