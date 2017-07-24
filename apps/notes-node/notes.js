a7d993187eda595a1726110125d25e6f1ff35204
C. Jordan Ball III <cjordanball@gmail.com>
2017-07-24 16:29:16 -0400

    update

--- apps/notes-node/notes.js
+++ apps/notes-node/notes.js
@@ -1,75 +1,64 @@
 console.log('Starting notes.js');
+const fs = require('fs');
 
-module.exports.age = 25;
+const fetchNotes = () => {
+	try {
+		let noteString = fs.readFileSync('./notes.txt', 'utf-8');
+		notes = JSON.parse(noteString);
+		return notes;
+	} catch(err) {
+		return [];
+	}
+};
 
-module.exports.addNote = () => {
-	console.log('addNote');
-	return 'New note.';
+const saveNotes = (notes) => {
+	fs.writeFileSync('./notes.txt', JSON.stringify(notes));
+};
+
+const addNote = (title, body) => {
+	let notes = fetchNotes();
+	let note = {
+		title,
+		body
+	}
+
+	let isUniq = !(notes.some(val => val.title === title));
+
+	if (isUniq) {
+		notes.push(note);
+		saveNotes(notes);
+		return note;
+	}
+};
+
+let getAll = () => {
+	return fetchNotes();
+};
+
+let getNote = (title) => {
+	let notes = fetchNotes();
+	let hotIndex = notes.findIndex(val => val.title === title);
+	if (hotIndex > -1) {
+		return notes[hotIndex];
+	}
+	return false;
 }
 
-module.exports.add = (num1, num2) => {
-	console.log(num1 + num2);
-};
-//const fs = require('fs');
-//
-//const fetchNotes = () => {
-//	try {
-//		let noteString = fs.readFileSync('./notes.txt', 'utf-8');
-//		notes = JSON.parse(noteString);
-//		return notes;
-//	} catch(err) {
-//		return [];
-//	}
-//};
-//
-//const saveNotes = (notes) => {
-//	fs.writeFileSync('./notes.txt', JSON.stringify(notes));
-//};
-//
-//const addNote = (title, body) => {
-//	let notes = fetchNotes();
-//	let note = {
-//		title,
-//		body
-//	}
-//
-//	let uniq = !(notes.some(val => val.title === title));
-//
-//	if (uniq) {
-//		notes.push(note);
-//		saveNotes(notes);
-//		return note;
-//	}
-//};
-//
-//let getAll = () => {
-//	return fetchNotes();
-//};
-//
-//let getNote = (title) => {
-//	let notes = fetchNotes();
-//	let hotIndex = notes.findIndex(val => val.title === title);
-//	if (hotIndex > -1) {
-//		return notes[hotIndex];
-//	}
-//	return false;
-//}
-//
-//let removeNote = (title) => {
-//	let notes = fetchNotes();
-//	let hotIndex = notes.findIndex(val => val.title === title);
-//	if (hotIndex > -1) {
-//		notes.splice(hotIndex, 1);
-//		saveNotes(notes);
-//		return true;
-//	}
-//	return false;
-//}
-//
-//
-//module.exports = {
-//	addNote,
-//	getAll,
-//	getNote,
-//	removeNote
-//}
+let removeNote = (title) => {
+	let notes = fetchNotes();
+	let hotIndex = notes.findIndex(val => val.title === title);
+	if (hotIndex > -1) {
+		notes.splice(hotIndex, 1);
+		saveNotes(notes);
+		return true;
+	}
+	return false;
+}
+
+
+module.exports = {
+	addNote,
+	getAll,
+	getNote,
+	removeNote
+}
