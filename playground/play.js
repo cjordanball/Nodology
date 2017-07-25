fdcb142695e7d85c6f74d02cdb5af085e6dbf99d
C. Jordan Ball III <cjordanball@gmail.com>
2017-07-25 14:30:29 -0400

    add notes on node debugging

--- playground/play.js
+++ playground/play.js
@@ -1,16 +1,5 @@
-let arr = ['jordan', 'jay', 'thompson'];
+const assert = require('assert');
 
-let names = {
-	name1: 'jordan',
-	name2: 'jay',
-	name3: 'thompson',
-	[Symbol.iterator]: function* () {
-		yield this.name2,
-		yield this.name1,
-		yield this.name3
-	}
-}
+let x = assert(false);
 
-for (let x of names) {
-	console.log(x);
-}
+console.log('x', x);
